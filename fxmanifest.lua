22f5271e685f64cf3cd638b5c1eaa396f6879aa8
Walrus <81704984+nagaming-master@users.noreply.github.com>
2023-01-29 21:49:42 -0600

    Sync repo with recent changes

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,25 +2,26 @@ fx_version 'cerulean'
 game 'gta5'
 
 description 'QB-MechanicJob'
-version '2.0.1'
+version '2.1.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'locales/en.lua', -- Edit this for your language.
-	'config.lua'
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua',
+    'locales/*.lua',
+    'config.lua',
 }
 
 client_scripts {
-	'client/main.lua',
-	'client/drivingdistance.lua',
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/CircleZone.lua',
+    'client/main.lua',
+    'client/drivingdistance.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/CircleZone.lua',
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 lua54 'yes'
